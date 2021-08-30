50ee47962df571bda7ee6ff9f8b0c1531a326585
Pooya Nasiri <pooya.nasiri75@gmail.com>
2021-08-30 22:27:55 +0430

    Add Common layer as a .net core class library Add DTOs: -ResultDto -ResultDtp<T>

---
+++ Common/DTOs/ResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.DTOs
+{
+    public class ResultDto
+    {
+        public string Message { get; set; }
+        public bool Status { get; set; }
+    }
+    public class ResultDto<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public bool Status { get; set; }
+    }
+}
